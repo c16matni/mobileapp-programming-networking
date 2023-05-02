7adf240ce637ccb0fbd9062a8c7c539fe4737a62
SleepyBearIV <mattias.nicander.mn@gmail.com>
2023-05-02 12:31:52 +0200

    Arraylist med tillhörande class och RecyclerView

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,14 @@
+package com.example.networking;
+
+public class Mountain {
+    private String mName;
+
+    public Mountain(String mName) {
+        this.mName = mName;
+    }
+
+    public String getName(){
+        return mName;
+    }
+
+}
